babc39a49af96f9076ce4b9b071375308f3898a6
zafarbekboltayev <boltayevzafarbek903@gmail.com>
2024-11-25 17:35:38 +0500

    capitan

--- script.js
+++ script.js
@@ -126,3 +126,15 @@ document.addEventListener("DOMContentLoaded", () => {
         observer.observe(card);
     });
 });
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
