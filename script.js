f45a5241684b285e4cfff6b269c75eef170613f8
zafarbekboltayev <boltayevzafarbek903@gmail.com>
2025-02-01 12:42:53 +0500

    qish yoq

--- script.js
+++ script.js
@@ -138,20 +138,3 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
-const snowflakesContainer = document.querySelector('.snowflakes');
-
-        function createSnowflake() {
-            const snowflake = document.createElement('div');
-            snowflake.classList.add('snowflake');
-            snowflake.textContent = '❄';
-            snowflake.style.left = Math.random() * 100 + 'vw';
-            snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
-            snowflake.style.fontSize = Math.random() * 10 + 10 + 'px';
-            snowflakesContainer.appendChild(snowflake);
-
-            setTimeout(() => {
-                snowflake.remove();
-            }, 5000);
-        }
-
-        setInterval(createSnowflake, 1);
